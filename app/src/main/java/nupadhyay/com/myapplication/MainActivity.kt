b7a0fb9dc457dc909c1eae14479c8a06c0a07857
Upadhyay, Niraj <Niraj.Upadhyay@cengage.com>
2018-11-12 15:52:24 -0500

    added name

--- app/src/main/java/nupadhyay/com/myapplication/MainActivity.kt
+++ app/src/main/java/nupadhyay/com/myapplication/MainActivity.kt
@@ -3,10 +3,11 @@ package nupadhyay.com.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class cMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var name = "niraj"
     }
 }
